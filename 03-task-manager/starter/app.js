ed858b47da428b5fe817a9ef834f0686c4eb11e9
olgakahraman <ol.khrmn@gmail.com>
2025-04-28 18:40:02 -0400

    week-6 homework is ready

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -3,16 +3,18 @@ const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
+const notFound = require('./middleware/not-found');
+const ErrorHandlerMiddleware = require('./middleware/error-handler');
 
+app.use(express.static('./public'));
 app.use(express.json());
-
-app.get('/hello', (req, res) => {
-  res.send('Task Manager App');
-});
 
 app.use('/api/v1/tasks', tasks);
 
-const port = 3000;
+app.use(notFound);
+app.use(ErrorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
